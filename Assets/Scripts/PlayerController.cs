cd3c26d8fb8cc9f6010d9e0ac0987d3bd1ffcdc9
xCoA <17sschuh@go.dsdmail.net>
2017-02-02 13:32:29 -0700

    thing thing

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text WinText;
+    public particleplayer trail; 
+
+    private Rigidbody rb;
+    private int count;
+
+
+    void Start ()
+    {
+
+        rb = GetComponent <Rigidbody>();
+        count = 0;
+        SetCountText();
+        WinText.text = "";
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+            trail.Upgrade();
+        }
+    }
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 10)
+        {
+            WinText.text = "You Win!";
+
+        }
+        if(count == 10)
+        {
+            SceneManager.LoadScene("Mini game");
+
+        }
+
+    }
+
+
+}
+
---
+++ Assets/particleplayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class particleplayer : MonoBehaviour {
+
+    private Vector3 offset;
+    private ParticleSystem ps;
+
+    public GameObject player;
+    public float lifeTime = 0.1f;
+    public float lengthmodifier = 1.5f;
+  
+    // Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+        ps = gameObject.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
+        transform.position = player.transform.position+offset;
+    }
+    public void Upgrade()
+    {
+
+        //var ma = ps.main;
+        lifeTime = lifeTime * lengthmodifier;
+        ps.startLifetime = lifeTime;
+        Debug.Log(lifeTime);
+    }
+
+}
